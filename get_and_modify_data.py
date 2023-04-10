9cd851b849a733aa163dd0e5749527e54c5832c3
Kay <anuforok@gmail.com>
2023-04-10 15:08:41 -0500

    hash attributes

--- get_and_modify_data.py
+++ get_and_modify_data.py
@@ -12,11 +12,40 @@ def main(os_path, openf, argv):
         config_fp = openf(config_fn)
         config.readfp(config_fp, filename=config_fn)
 
-        kumc_api = config.get('api', 'kumc_redcap_api_url')
-        verify_ssl = config.getboolean('api', 'verify_ssl')
-        file_dest = config.get(pid, 'file_dest')
+        values = {}
 
-        return kumc_api, verify_ssl, file_dest, config_fn, pid, os_path
+        # default attributes
+        values['import_dir'] = config.get('default', 'import_dir')
+        values['export_dir'] = config.get('default', 'export_dir')
+        values['data_content'] = config.get('default', 'data_content')
+        values['data_event'] = config.get('default', 'data_event')
+        values['data_metadata'] = config.get('default', 'data_metadata')
+        values['data_action_export'] = config.get('default', 'data_action_export')
+        values['data_action_import'] = config.get('default', 'data_action_import')
+        values['data_format'] = config.get('default', 'data_format')
+        values['data_type'] = config.get('default', 'data_type')
+        values['data_delimiter'] = config.get('default', 'data_delimiter')
+        values['data_label'] = config.get('default', 'data_label')
+        values['data_true'] = config.getboolean('default', 'data_true')
+        values['data_return_count'] = config.get('default', 'data_return_count')
+        values['data_return_format'] = config.get('default', 'data_return_format')
+        values['data_overwrite'] = config.get('default', 'data_overwrite')
+        values['data_date_format'] = config.get('default', 'data_date_format')
+        values['log_format'] = config.get('default', 'log_format')
+        values['log_datefmt'] = config.get('default', 'log_datefmt')
+        
+        # api
+        values['kumc_redcap_api_url'] = config.get('api', 'kumc_redcap_api_url')
+        values['chld_redcap_api_url'] = config.get('api', 'chld_redcap_api_url')
+        values['verify_ssl'] = config.getboolean('api', 'verify_ssl')
+        
+        # tokens
+        values['token_kumc'] = config.get(pid, 'token_kumc')
+        values['token_chld'] = config.get(pid, 'token_chld')
+        values['proj_token'] = config.get(pid, 'proj_token')
+        values['file_dest'] = config.get(pid, 'file_dest')
+
+        return values
     return get_config()
         
 
@@ -28,12 +57,8 @@ if __name__ == "__main__":
         from __builtin__ import open as openf
 
         config_values = main(os_path, openf, argv)
-        print(config_values[0])
-        print(config_values[1])
-        print(config_values[2])
-        print(config_values[3])
-        print(config_values[4])
-        print(config_values[5])
-       
+        print(config_values['kumc_redcap_api_url'])
+        print(config_values['chld_redcap_api_url'])
+        print(config_values['verify_ssl'])
 
     _main_ocap()
