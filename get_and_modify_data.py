1564b433dbf10f29fecd3deb9566aac23bc00025
Kay <anuforok@gmail.com>
2023-04-18 15:27:19 -0500

    trace is_extenstion issue

--- get_and_modify_data.py
+++ get_and_modify_data.py
@@ -110,11 +110,11 @@ def mapped_headers():
         site_csv_list.append(site_data_col_renamed_df)
 
     
-    # merge all the sites csvs
-    merge_site_cvs = pd.concat([site_csv_list[0], site_csv_list[1], site_csv_list[2]], sort=False)
+    # # merge all the sites csvs
+    # merge_site_cvs = pd.concat([site_csv_list[0], site_csv_list[1], site_csv_list[2]], sort=False)
 
     # return merged file
-    return merge_site_cvs
+    return site_csv_list
 
 def main(os_path, openf, argv):
     def get_config():
