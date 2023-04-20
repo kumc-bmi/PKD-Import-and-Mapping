13a076792f930626d051fcd72e4896264d2afd5d
Kay <anuforok@gmail.com>
2023-04-20 17:43:08 -0500

    view columns

--- get_and_modify_data.py
+++ get_and_modify_data.py
@@ -124,6 +124,7 @@ def mapped_headers():
             # check if column is in site data frame
             if col in site_data_col_renamed_df.columns:
                 print(df_mapped)
+                print(col)
                 # If it does exist, map values using source mapping dictionary
                 df_mapped[col] = [mapping.get(val, val) for val in site_data_col_renamed_df[col]]
             else:
