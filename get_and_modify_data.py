51179d78a51f41ca27d9deffb43c1991d533b706
Kay <anuforok@gmail.com>
2023-06-29 13:36:02 -0500

    remove log redundancy

--- get_and_modify_data.py
+++ get_and_modify_data.py
@@ -567,12 +567,13 @@ def redcap_export_api():
             token = token_kumc
             project_id = kumc_project_id
             api_url = kumc_api_url
-            log_details.debug('API URL: %s', api_url)
         elif folder == 'uab':
             token = token_chld
             project_id = chld_project_id
             api_url = cri_api_url
-            log_details.debug('API URL: %s', api_url)
+        
+        # log details
+        log_details.debug('API URL: %s', api_url)
 
         # data parameters
         data_param = {
