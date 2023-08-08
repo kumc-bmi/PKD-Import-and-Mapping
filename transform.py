3d5d553ab1c0f41831191033553a7c4c3a949094
Kay <anuforok@gmail.com>
2023-08-08 12:10:02 -0500

    fix export csv site data renamed

--- transform.py
+++ transform.py
@@ -666,7 +666,7 @@ def mapped_csvs():
             
             print(site_data_col_renamed_df)
 
-            site_source_mapping.to_csv(import_directory + site + '/' + "site_data_col_renamed" + site + '.csv', index=False, float_format=None)
+            site_data_col_renamed_df.to_csv(import_directory + site + '/' + "site_data_col_renamed" + site + '.csv', index=False, float_format=None)
 
             # create new dataframe and apply the mapping to the column with values to be converted
             for col in site_data_col_renamed_df.columns:
