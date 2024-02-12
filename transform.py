3ceeb347f69c2984688b45ae5ea3489cb12c9eaf
Kelechi Anuforo <anuforok@gmail.com>
2024-02-12 16:02:03 -0600

    strip white spaces in date columns

--- transform.py
+++ transform.py
@@ -522,16 +522,16 @@ def mapped_csvs():
                 else:
                     redcap_event_name = row['crvisit']
 
-                if 'crrdate' in row.index and 'cr4' in row.index and pd.notna(row['crrdate']) and pd.notna(row['cr4']) and row['crrdate'] != '' and row['cr4'] != '':
+                if 'crrdate' in row.index and 'cr4' in row.index and pd.notna(row['crrdate'].strip()) and pd.notna(row['cr4'].strip()) and row['crrdate'].strip() != '' and row['cr4'].strip() != '':
                     print(site)
-                    print(row['crrdate'], row['cr4'])
-                    age = str((datetime.strptime(row['crrdate'], '%m/%d/%Y')).year - (datetime.strptime(row['cr4'], '%m/%d/%Y')).year)
+                    print(row['crrdate'].strip(), row['cr4'].strip())
+                    age = str((datetime.strptime(row['crrdate'].strip(), '%m/%d/%Y')).year - (datetime.strptime(row['cr4'].strip(), '%m/%d/%Y')).year)
                     print(age)
                 else:
                     age = ''
 
-                if 'crrdate' in row.index and 'cr4' in row.index and pd.notna(row['crrdate']) and pd.notna(row['cr4']) and row['crrdate'] != '' and row['cr4'] != '':
-                    pmhhtn_age_onset = str((datetime.strptime(row['crrdate'], '%m/%d/%Y')).year - (datetime.strptime(row['cr4'], '%m/%d/%Y')).year)
+                if 'crrdate' in row.index and 'cr4' in row.index and pd.notna(row['crrdate'].strip()) and pd.notna(row['cr4'].strip()) and row['crrdate'].strip() != '' and row['cr4'].strip() != '':
+                    pmhhtn_age_onset = str((datetime.strptime(row['crrdate'].strip(), '%m/%d/%Y')).year - (datetime.strptime(row['cr4'].strip(), '%m/%d/%Y')).year)
                 else:
                     pmhhtn_age_onset = ''
 
@@ -544,8 +544,8 @@ def mapped_csvs():
                 else:
                     birth_weight = ''
 
-                if  'cr28a' in row.index and 'cr4' in row.index and pd.notna(row['cr28a']) and pd.notna(row['cr4']) and row['crrdate'] != '' and row['cr4'] != '':
-                    rpmenopage = str((datetime.strptime(row['cr28a'], '%m/%d/%Y')).year - (datetime.strptime(row['cr4'], '%m/%d/%Y')).year)
+                if  'cr28a' in row.index and 'cr4' in row.index and pd.notna(row['cr28a'].strip()) and pd.notna(row['cr4'].strip()) and row['cr28a'].strip() != '' and row['cr4'].strip() != '':
+                    rpmenopage = str((datetime.strptime(row['cr28a'].strip(), '%m/%d/%Y')).year - (datetime.strptime(row['cr4'].strip(), '%m/%d/%Y')).year)
                 else:
                     rpmenopage = ''
 
@@ -570,8 +570,8 @@ def mapped_csvs():
                 else:
                     smokever = ''
 
-                if 'hb30' in row.index and row['hb30'] == 'yes' and row['crrdate'] != '' and row['cr4'] != '':
-                    sualcodur = str((datetime.strptime(row['crrdate'], '%m/%d/%Y')).year - (datetime.strptime(row['cr4'], '%m/%d/%Y')).year)
+                if 'hb30' in row.index and row['hb30'] == 'yes' and row['crrdate'].strip() != '' and row['cr4'].strip() != '':
+                    sualcodur = str((datetime.strptime(row['crrdate'].strip(), '%m/%d/%Y')).year - (datetime.strptime(row['cr4'].strip(), '%m/%d/%Y')).year)
                 elif 'hb29a' in row.index and 'hb31a' in row.index and 'hb30' in row.index and row['hb30'] == 'no' and row['hb31a'] != '' and row['hb29a'] != '':
                     sualcodur = str((datetime.strptime(row['hb31a'], '%m/%d/%Y')).year - (datetime.strptime(row['hb29a'], '%m/%d/%Y')).year)
                 else:
@@ -608,8 +608,8 @@ def mapped_csvs():
                 else:
                     height_m = ''
                     
-                if 'hb3' in row.index and pd.notna(row['crrdate']) and row['crrdate'] != '' and pd.notna(row['cr4']) and row['cr4'] != '' and row['hb3'] == 'yes' and row['crrdate'] != '' and row['cr4'] != '':                    
-                    sucigdur = str((datetime.strptime(row['crrdate'], '%m/%d/%Y')).year - (datetime.strptime(row['cr4'], '%m/%d/%Y')).year - pd.to_numeric(row['hb2a'], errors='coerce')) 
+                if 'hb3' in row.index and pd.notna(row['crrdate'].strip()) and row['crrdate'].strip() != '' and pd.notna(row['cr4'].strip()) and row['cr4'].strip() != '' and row['hb3'] == 'yes' and row['crrdate'].strip() != '' and row['cr4'].strip() != '':                    
+                    sucigdur = str((datetime.strptime(row['crrdate'].strip(), '%m/%d/%Y')).year - (datetime.strptime(row['cr4'].strip(), '%m/%d/%Y')).year - pd.to_numeric(row['hb2a'], errors='coerce')) 
                 elif 'hb3' in row.index and pd.notna(row['hb3']) and row['hb3'] != '' and row['hb3'] == 'no':
                     sucigdur = str(pd.to_numeric(row['hb5a'], errors='coerce') - pd.to_numeric(row['hb2a'], errors='coerce'))
                 else:
